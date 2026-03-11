ff5470e66675471b6d99f2cd3888a7231cba5e1c
praiseuser <159968121+praiseuser@users.noreply.github.com>
2026-03-11 12:05:48 +0100

    fix about import

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,7 +3,7 @@ import StatsBand from '../../components/About/StatsBand';
 import MissionSection from '../../components/About/MissionSection';
 import ValuesSection from '../../components/About/ValuesSection';
 import TeamSection from '../../components/About/TeamSection';
-import TimelineSection from '../../components/About/TimelineSection';
+import TimelineSection from '../../components/About/TimeLineSection';
 import CTAStrip from '../../components/About/CTAStrip';
 
 export default function AboutPage() {
